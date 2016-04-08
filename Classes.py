6556c01aeb79c35a828f2d84ea2c51900287abae
srvrana <srvrana@users.noreply.github.com>
2016-04-07 20:10:14 -0500

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -24,16 +24,3 @@ class Teacher:
         self.startTime = ""
         self.endTime = ""
         self.homeRoom = ""  #String
-=======
-class Teacher:
-	name = ""
-	type = ""
-	designation = ""
-	startTime = ""
-	endTime = ""
-	#Will be array of blocks
-	schedule = []
-	# avail is which periods teacher is available, 
-	# for middle school 1,2,3 are only options
-	avail = []
-	subjects = []
