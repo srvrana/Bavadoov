eaa052fb165b8aeaa3eed88f07602d92a6d6760e
jldonahoe <jldonahoe@unomaha.edu>
2016-05-04 21:30:51 -0500

    Sikuli test scripts

---
+++ TestingFiles/Sikuli/version1_0.sikuli/test3.py
@@ -0,0 +1,74 @@
+click("1460848701303.png")
+wait("1460848740522.png")
+click("1460848740522.png")
+wait("1460848791776.png")
+type("testFile1")
+click("1460848791776.png")
+wait("1460848791776.png")
+type("5")
+click("1460848878433.png")
+click(Pattern("1462283555616.png").targetOffset(0,-302))
+type("Adam" + Key.TAB)
+type("Algebra" + Key.TAB)
+type("1,2,3,4,5,6" + Key.TAB)
+type("1" + Key.TAB)
+type("Betty" + Key.TAB)
+type("Biology" + Key.TAB)
+type("1,2,3,5,6" + Key.TAB)
+type("1" + Key.TAB)
+type("Charlie" + Key.TAB)
+type("Chemistry" + Key.TAB)
+type("1,2,3,4,5,6" + Key.TAB)
+type("2" + Key.TAB)
+type("Donna" + Key.TAB)
+type("Dance" + Key.TAB)
+type("1,2,3,4,5" + Key.TAB)
+type("1" + Key.TAB)
+type("Edward" + Key.TAB)
+type("English" + Key.TAB)
+type("1,2,3,4,5,6" + Key.TAB)
+type("3")
+click("1460853661214.png")
+click("1462377143789.png")
+click("1462377143789.png")
+click("1462391997354.png")
+
+type("Algebra" + Key.TAB)
+type("7B,8A" + Key.TAB + Key.SPACE)
+click("1462392008698.png")
+
+
+
+click(Pattern("1462297013756.png").targetOffset(-23,-8))
+
+
+type("Biology" + Key.TAB)
+type("6A,6B,7A,7B")
+click(Pattern("1462284190351.png").targetOffset(-77,82))
+
+click(Pattern("1462377027712.png").targetOffset(-25,-68))
+
+
+type("Chemistry 1" + Key.TAB)
+type("6A,6B,7A" + Key.TAB + Key.TAB)
+type("Chemistry 2" + Key.TAB)
+type("7B,8A,8B")
+click(Pattern("1462284243581.png").targetOffset(-77,142))
+
+click("1462377612664.png")
+
+
+type("Dance" + Key.TAB)
+type("6A,6B")
+click(Pattern("1462377419876.png").targetOffset(-78,76))
+
+
+click(Pattern("1462283662653.png").targetOffset(-23,-127))
+type("English 1" + Key.TAB)
+type("6A,6B" + Key.TAB + Key.TAB)
+type("English 2" + Key.TAB)
+type("7A,7B" + Key.TAB + Key.TAB)
+type("English 3" + Key.TAB)
+type("8A,8B")
+click(Pattern("1462284336158.png").targetOffset(-73,200))
+
